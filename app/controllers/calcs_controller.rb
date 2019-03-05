d143c3bbad171461175025f5bfe73dd4a95c65c7
masahiro-g <kwoaudis32@gmail.com>
2019-03-05 15:09:48 +0900

    平日課題4 機能追加完了

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,2 +1,15 @@
 class CalcsController < ApplicationController
+  def show
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+    if params[:calcs] == "addition"
+      @result = number1 + number2
+    elsif params[:calcs] == "subtraction"
+      @result = number1 - number2
+    elsif params[:calcs] == "multiplication"
+      @result = number1 * number2
+    elsif params[:calcs] == "division"
+      @result = number1 / number2
+    end
+  end
 end
