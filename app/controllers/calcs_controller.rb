2609eaf3c4ac7be9d56c3f1d1518437c17456a1c
masahiro-g <kwoaudis32@gmail.com>
2019-03-07 16:44:47 +0900

    課題4機能実装 例外処理

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,15 +1,7 @@
 class CalcsController < ApplicationController
+  include Common
+  
   def show
-    number1 = params[:number1].to_i
-    number2 = params[:number2].to_i
-    if params[:calcs] == "addition"
-      @result = number1 + number2
-    elsif params[:calcs] == "subtraction"
-      @result = number1 - number2
-    elsif params[:calcs] == "multiplication"
-      @result = number1 * number2
-    elsif params[:calcs] == "division"
-      @result = number1 / number2
-    end
+    calculation
   end
 end
---
+++ app/controllers/concerns/common.rb
@@ -0,0 +1,30 @@
+module Common
+  extend ActiveSupport::Concern
+  
+  def calculation
+  num1 = params[:num1].to_i
+  num2 = params[:num2].to_i
+  operator = params[:operator]
+
+  @result = case operator
+            when "addition"
+              num1 + num2
+            when "subtraction" 
+              num1 - num2
+            when "multiplication" 
+              num1 * num2
+            when "division"
+            begin 
+              if num1 % num2 == 0
+              "#{num1 / num2}"
+              else
+              "#{num1 / num2}..#{num1 % num2}"
+              end
+            rescue ZeroDivisionError 
+              "divided by 0"
+            end
+          else
+            "計算が正しくありません"
+          end
+  end
+end
