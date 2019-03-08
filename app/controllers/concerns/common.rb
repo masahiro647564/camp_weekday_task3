829dff314124b171ecef04e37adfa25bae397354
masahiro-g <kwoaudis32@gmail.com>
2019-03-08 11:21:47 +0900

    common.rbの機能一部改善

--- app/controllers/concerns/common.rb
+++ app/controllers/concerns/common.rb
@@ -2,29 +2,28 @@ module Common
   extend ActiveSupport::Concern
   
   def calculation
-  num1 = params[:num1].to_i
-  num2 = params[:num2].to_i
-  operator = params[:operator]
+    num1 = params[:num1].to_i
+    num2 = params[:num2].to_i
+    operator = params[:operator]
 
-  @result = case operator
-            when "addition"
-              num1 + num2
-            when "subtraction" 
-              num1 - num2
-            when "multiplication" 
-              num1 * num2
-            when "division"
-            begin 
-              if num1 % num2 == 0
-              "#{num1 / num2}"
-              else
-              "#{num1 / num2}..#{num1 % num2}"
+    @result = case operator
+              when "addition"
+                num1 + num2
+              when "subtraction" 
+                num1 - num2
+              when "multiplication" 
+                num1 * num2
+              when "division"
+              begin 
+                if num1 % num2 == 0
+                "#{num1 / num2}"
+                else
+                "#{num1 / num2}..#{num1 % num2}"
+                end
+              rescue ZeroDivisionError 
+                "divided by 0"
               end
-            rescue ZeroDivisionError 
-              "divided by 0"
             end
-          else
-            "URLが正しくありません"
-          end
+            
   end
 end
